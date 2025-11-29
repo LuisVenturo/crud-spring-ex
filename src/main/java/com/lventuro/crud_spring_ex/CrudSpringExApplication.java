b025d90589c7836cea1814adbb70bbc12af0af1d
LuisVenturo <venturomoralesluis@gmail.com>
2025-11-28 22:20:11 -0500

    my first commit

---
+++ src/main/java/com/lventuro/crud_spring_ex/CrudSpringExApplication.java
@@ -0,0 +1,13 @@
+package com.lventuro.crud_spring_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lventuro/crud_spring_ex/CrudSpringExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lventuro.crud_spring_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
